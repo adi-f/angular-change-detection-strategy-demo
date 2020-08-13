a4aba32e7a82379ac5d82e2d25d03cce74614008
adi-f <adrian.fahrer@mimacom.com>
2020-08-13 08:15:46 +0200

    1 file modified

--- src/with-async/with-async.component.ts
+++ src/with-async/with-async.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-with-async',
+  selector: 'with-async',
   templateUrl: './with-async.component.html'
 })
 export class WithAsyncComponent {
+
   @Input() counter$: Observable<number>;
 }
