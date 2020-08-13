e22245577e59a7fe9a3d8e15f0b0cdbe61f74db7
adi-f <adrian.fahrer@mimacom.com>
2020-08-13 09:06:51 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,33 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, OnDestroy } from '@angular/core';
 import { Subject, BehaviorSubject, ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html'
 })
-export class AppComponent  {
+export class AppComponent implements OnDestroy {
+  
   counter: number = 1; 
   readonly counter$: Subject<number> = new ReplaySubject(1);
 
+  private setIntervalHandle: any = null
+
+
   increment(): void {
-    this.counter$.next(this.counter++);
+    this.counter$.next(++this.counter);
   }
 
   autoIncrement(): void {
-    setInterval(() => this.counter$.next(this.counter++), 1000);
+    this.setIntervalHandle = setInterval(() => this.counter$.next(++this.counter), 1000);
+  }
+
+  private stopInterval(): void {
+    if(this.setIntervalHandle) {
+      clearInterval(this.setIntervalHandle);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.stopInterval();
   }
 }
